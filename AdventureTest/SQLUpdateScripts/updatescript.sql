4f08995ed20558e28b66e38c587a04a64bc79e48
AjayKumarBanala <104990946+AjayKumarBanala@users.noreply.github.com>
2024-04-22 12:41:51 +0530

    Update updatescript.sql

--- AdventureTest/SQLUpdateScripts/updatescript.sql
+++ AdventureTest/SQLUpdateScripts/updatescript.sql
@@ -1,4 +1,2 @@
-update [AdventureWorks2019].[HumanResources].[Department] set ModifiedDate=Getdate();
-go
-Alter table [AdventureWorks2019].[HumanResources].[Department] add Test1 Varchar(10);
+Alter table [AdventureWorks2019].[HumanResources].[Department] add Test2 Varchar(10);
 go
